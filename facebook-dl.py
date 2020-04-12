63962d41ff7d024d3d3dc03afe5e1aebd1bad1e4
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2020-04-12 11:13:12 +0200

    cleaned up the code
    - use requests instead of urllib
    - added docstring
    - use f-strings

--- facebook-dl.py
+++ facebook-dl.py
@@ -1,64 +1,69 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from time import sleep
 
 import re
 import sys
-import requests
-import urllib.request
 import random
 import argparse
+import requests
 
 good = "\033[92m✔\033[0m"
 
-# This magic spell lets me erase the current line. 
+# This magic spell lets me erase the current line.
 # I can use this to show for example "Downloading..."
-# and then "Downloaded" on the line where 
-# "Downloading..." was.  
+# and then "Downloaded" on the line where
+# "Downloading..." was.
 ERASE_LINE = '\x1b[2K'
 
 # This extracts the video url
 def extract_url(html, quality):
+    """
+    html       -    the html code of the webpage where the video is located
+    quality    -    the quality of the video that should be downloaded (SD or HD)
+    """
+    if quality == "sd":
+        # Standard Definition video
+        url = re.search('sd_src:"(.+?)"', html)[0]
+    else:
+        # High Definition video
+        url = re.search('hd_src:"(.+?)"', html)[0]
 
-	if quality == "sd":
-		# Standard Definition video
-		url = re.search('sd_src:"(.+?)"', html)[0]
-	else:
-		# High Definition video
-		url = re.search('hd_src:"(.+?)"', html)[0]
+        # cleaning the url
+        url = url.replace('hd_src:"', '')
+        url = url.replace('sd_src:"', '')
+        url = url.replace('"', "")
 
-	# cleaning the url 
-	url = url.replace('hd_src:"', '')
-	url = url.replace('sd_src:"', '')
-	url = url.replace('"', "")
-
-	return url
+    return url
 
 
 def main():
-	parser = argparse.ArgumentParser(description = "Download videos from facebook from your terminal")
-	
-	parser.add_argument('url', action="store")
-	parser.add_argument('resolution', action="store", nargs="?")
+    """
+    This function takes no arguments.
+    """
+    parser = argparse.ArgumentParser(description="Download videos from facebook from your terminal")
 
-	args = parser.parse_args()
+    parser.add_argument('url', action="store")
+    parser.add_argument('resolution', action="store", nargs="?")
 
-	print("Fetching source code...", end="\r", flush=True)
-	r = requests.get(args.url)
-	sys.stdout.write(ERASE_LINE)
-	print(good, "Fetched source code")
+    args = parser.parse_args()
 
-	file_url = extract_url(r.text, args.resolution)
+    print("Fetching source code...", end="\r", flush=True)
+    request = requests.get(args.url)
+    sys.stdout.write(ERASE_LINE)
+    print(good, "Fetched source code")
 
-	# Generates a random number with will be the file name
-	path = str(random.random())[3:12] + ".mp4"
+    file_url = extract_url(request.text, args.resolution)
 
-	print("Downloading video...",  end="\r", flush=True)
-	# Downloads the video
-	urllib.request.urlretrieve(file_url, path)
-	sys.stdout.write(ERASE_LINE)
-	print(good, "Video downloaded:", path)
+    # Generates a random number with will be the file name
+    path = f"{str(random.random())[3:12]}.mp4"
+    print("Downloading video...", end="\r", flush=True)
+    # Downloads the video
+    request = requests.get(file_url)
+    
+    with open(path, "wb") as f:
+        f.write(request.content)
 
-if __name__=="__main__":
-	main()
+    sys.stdout.write(ERASE_LINE)
+    print(f"{good} Video downloaded: {path}")
+
+if __name__ == "__main__":
+    main()
