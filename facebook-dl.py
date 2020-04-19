b80189c8c3dc402cc1fed629723eece440e3c31b
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2020-04-19 12:24:11 +0200

    added more comments to make the code more clear

--- facebook-dl.py
+++ facebook-dl.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 #
 # pylint: disable=missing-docstring,trailing-whitespace,invalid-name
+#
+# By Siddharth Dushantha (sdushantha) 2020
+#
+# Notice: If you are on Windows, the output of this script might
+#         look a little messy and that is because command prompt
+#         does not support escape sequences.
+#
+
 
 import re
 import sys
@@ -11,7 +19,7 @@ import requests
 # I can use this to show for example "Downloading..."
 # and then "Downloaded" on the line where
 # "Downloading..." was.
-ERASE_LINE = '\x1b[2K'
+ERASE_LINE = '\e[2K'
 
 def main():
     parser = argparse.ArgumentParser(description="Download videos from facebook from your terminal")
@@ -42,7 +50,7 @@ def main():
         request = requests.get(re.findall(f"{'sd_src' if args.resolution == 'sd' else 'hd_src'}:\"(.+?)\"", request.text)[0])
     except IndexError:
         sys.stdout.write(ERASE_LINE)
-        print("\033[91m✘\033[0m Video could not be downloaded")
+        print("\e[91m✘\e[0m Video could not be downloaded")
         sys.exit()
 
     # Write the content to the file
@@ -50,6 +58,14 @@ def main():
         f.write(request.content)
 
     print(ERASE_LINE, end="\r", flush=True)
+
+    # You might be wondering why in this line the ansi code starts with
+    # \033, but all other ones starts with \e.
+    # This is because \e for some bizzar reason does not work with f-strings.
+    # I did try doing a quick Google search, but did not find anything so thats
+    # why I am equally confused as you are to why \e wont work.
+    # If you would like to learn more about \033 and \e take look at
+    # this link: https://unix.stackexchange.com/a/89817/342070
     print(f"\033[92m✔\033[0m Video downloaded: {file_name}")
 
 if __name__ == "__main__":
